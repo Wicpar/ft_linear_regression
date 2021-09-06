49841084c636931de830155bf9eaf48e7e033bc8
Frédéric Nieto <frederic.artus.nieto@gmail.com>
2021-09-06 18:33:31 +0200

    added a few tweaks

--- src/bin/train.rs
+++ src/bin/train.rs
@@ -5,7 +5,6 @@ use ft_linear_regression::dataset::{DatasetArg, Dataset, DatasetEntry};
 use ft_linear_regression::estimate_price::estimate_price;
 use std::time::Instant;
 
-
 pub struct LearnRatioArg;
 
 impl F64Parser<'_> for LearnRatioArg {
@@ -24,6 +23,10 @@ fn main() {
     let dataset_path = DatasetArg::try_parse(&args, &mut used);
     let ratio = LearnRatioArg::parse(&args, &mut used);
 
+    if ratio >= 1.0 || ratio <= 0.0 {
+        println!("Error: Learning ratio must be 0 < R < 1");
+    }
+
     for (idx, it) in args.iter().enumerate() {
         if !used[idx] {
             arg_err(idx, it, "Arg is not recognized, ignoring. --help for more info");
@@ -40,15 +43,16 @@ fn main() {
         let mut iter: usize = 0;
         let start = Instant::now();
         loop {
-            let last = theta;
             let tmp = normalized.entries.iter().map(|DatasetEntry { km, price }| {
                 // use the temporary t0 so we don't compute things twice
                 let t0 = estimate_price(*km, theta) - price;
                 (t0, t0 * km)
             }).reduce(|a, b| (a.0 + b.0, a.1 + b.1)).unwrap_or((0.0, 0.0));
+            let tmp = (tmp.0 * base, tmp.1 * base);
+            let last = theta;
             theta = (
-                theta.0 - base * tmp.0,
-                theta.1 - base * tmp.1,
+                last.0 - tmp.0,
+                last.1 - tmp.1,
             );
             iter += 1;
             if (last.0 - theta.0).abs() == 0.0 && (last.1 - theta.1).abs() == 0.0 {
@@ -66,6 +70,7 @@ fn main() {
         }
 
         println!("Done {} iterations in {:.3}s", iter, start.elapsed().as_secs_f64());
+        println!("Theta is {:?}", theta);
         //normalized.draw_to_file_with_theta("normalized.png", theta);
         //raw.draw_to_file_with_theta("raw.png", raw.denormalize_theta(theta));
 
